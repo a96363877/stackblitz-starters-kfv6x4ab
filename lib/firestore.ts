9f473c42faaffc8156ca09f001075cbe01d4832f
a96363877 <a96363877@gmail.com>
2025-02-04 12:48:44 -0800

    srdf

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -3,12 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCEK4irWtP1f6y_J1Gj3kNwsyLdItndt5Y",
+  authDomain: "sdkk-22bbc.firebaseapp.com",
+  projectId: "sdkk-22bbc",
+  storageBucket: "sdkk-22bbc.firebasestorage.app",
+  messagingSenderId: "1039314059862",
+  appId: "1:1039314059862:web:e14e764f77ce86c817b1f7",
+  measurementId: "G-K0L3MMPDR7"
+
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
